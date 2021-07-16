37bba361ad34fb77710ac685fd4d2352386303f5
Ilia Sidorenko <ilia.sidorenko.421@gmail.com>
2021-07-17 01:39:49 +1200

    move around env

--- ec2init-run.sh
+++ ec2init-run.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 set -ex
-. .env
 KEY="~/.ssh/devtest2020.pem"
 EC2IP="13.211.123.26"
 REPO="noway/thisprimedoesnotexist"
+. .env
 ssh -i "$KEY" ec2-user@"$EC2IP" sudo bash -s "$PAT" "$REPO" < ec2init.sh
