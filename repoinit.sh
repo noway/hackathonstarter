e2c769c79692a48ebca5043d85d2e0d43af65bff
Ilia Sidorenko <ilia.sidorenko.421@gmail.com>
2021-07-17 09:53:51 +1200

    switch to yarn + readme

--- repoinit.sh
+++ repoinit.sh
@@ -15,16 +15,16 @@ echo 'node_modules/' > .gitignore
 git add .gitignore
 git commit -m 'ignore node_modules'
 
-npm i express
-npm i -D @types/express
+yarn add express
+yarn add -D @types/express
 git add .
 git commit -m 'add express'
 
-npm i ts-node
+yarn add ts-node
 echo '{}' > tsconfig.json
-jq '.scripts.start = "ts-node --transpile-only index.ts"' package.json > tmp && mv tmp package.json
+jq '.scripts.prod = "ts-node --transpile-only index.ts"' package.json > tmp && mv tmp package.json
 git add .
-git commit -m 'add ts-node and start script'
+git commit -m 'add ts-node and prod script'
 
 cat > index.ts <<- EOF
 import * as express from 'express'
@@ -42,7 +42,26 @@ EOF
 git add .
 git commit -m 'add index.ts'
 
-npm i ts-node-dev
-jq '.scripts.dev = "tsnd --transpile-only --respawn --rs index.ts"' package.json > tmp && mv tmp package.json
+yarn add ts-node-dev
+jq '.scripts.start = "ts-node-dev --transpile-only --respawn --rs index.ts"' package.json > tmp && mv tmp package.json
 git add .
-git commit -m 'add ts-node-dev and dev script'
+git commit -m 'add ts-node-dev and start script'
+
+cat > README.md <<- EOF
+# $DIRNAME
+
+## Available Scripts
+
+In the project directory, you can run:
+
+### \`yarn start\`
+
+Runs the app in the development mode. \
+The server will restart when a file is modified.
+
+### \`yarn prod\`
+
+Runs the app in the production mode.
+EOF
+git add .
+git commit -m 'add README.md'
