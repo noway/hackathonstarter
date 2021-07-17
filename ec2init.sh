2024e77131245f10343d51513614a862a21f710a
Ilia Sidorenko <ilia.sidorenko.421@gmail.com>
2021-07-17 18:26:29 +1200

    static bin for caddy

--- ec2init.sh
+++ ec2init.sh
@@ -6,12 +6,24 @@ export DIRNAME="$(basename $REPO)"
 export PUBLIC_HOSTNAME="$(curl http://169.254.169.254/latest/meta-data/public-hostname)"
 export PUBLIC_IPV4="$(curl http://169.254.169.254/latest/meta-data/public-ipv4)"
 export TITLE="ec2-user@$(hostname) ($PUBLIC_HOSTNAME)"
-amazon-linux-extras install epel -y
 curl -fsSL https://rpm.nodesource.com/setup_current.x | bash -
 curl -fsSL https://dl.yarnpkg.com/rpm/yarn.repo | tee /etc/yum.repos.d/yarn.repo
-yum install -y git nodejs caddy htop yarn jq
+yum install -y git nodejs htop yarn jq
 npm install -g pm2
-cat > /etc/caddy/caddy.conf <<- EOF
+
+wget 'https://caddyserver.com/api/download?os=linux&arch=arm64' -O /usr/bin/caddy
+groupadd --system caddy
+useradd --system \
+    --gid caddy \
+    --create-home \
+    --home-dir /var/lib/caddy \
+    --shell /usr/sbin/nologin \
+    --comment "Caddy web server" \
+    caddy
+wget https://raw.githubusercontent.com/caddyserver/dist/master/init/caddy.service -O /etc/systemd/system/caddy.service
+systemctl daemon-reload
+systemctl enable caddy
+cat > /etc/caddy/Caddyfile <<- EOF
 $PUBLIC_HOSTNAME {
   reverse_proxy localhost:3000
 }
@@ -26,6 +38,7 @@ $PUBLIC_IPV4 {
 }
 EOF
 systemctl start caddy
+
 cd /home/ec2-user
 sudo -u ec2-user ssh-keyscan github.com | sudo -u ec2-user tee -a .ssh/known_hosts
 sudo -u ec2-user ssh-keygen -t ed25519 -f .ssh/id_ed25519 -P ""
@@ -38,6 +51,6 @@ sudo -u ec2-user curl \
 sudo -u ec2-user git clone "git@github.com:$REPO.git"
 cd "$DIRNAME/"
 sudo -u ec2-user yarn --frozen-lockfile
-sudo -u ec2-user pm2 start yarn --name "$DIRNAME" -- start
+sudo -u ec2-user pm2 start yarn --interpreter bash --name "$DIRNAME" -- start
 echo "https://$PUBLIC_IPV4"
 echo "https://$PUBLIC_HOSTNAME"
